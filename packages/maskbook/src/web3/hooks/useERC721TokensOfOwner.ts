c41fc2a6ab93689792bc49e9c8bc4a40f1efcdc7
guanbinrui <guanbinrui@dimension.im>
2020-10-29 14:54:46 +0800

    chore: add ERC721 hooks

--- packages/maskbook/src/web3/hooks/useERC721TokensOfOwner.ts
+++ packages/maskbook/src/web3/hooks/useERC721TokensOfOwner.ts
@@ -3,16 +3,21 @@ import { useERC721TokenContract } from '../contracts/useERC721TokenContract'
 import { EthereumTokenType, Token } from '../types'
 import { useAccount } from './useAccount'
 import { useChainId } from './useChainState'
+import { useTokenBalance } from './useTokenBalance'
 
-export function useERC721TokenOwnerByIndex(token?: PartialRequired<Token, 'address' | 'type'>, index: number = 0) {
+export function useERC721TokensOfOwner(token?: PartialRequired<Token, 'address' | 'type'>) {
     const chainId = useChainId()
     const account = useAccount()
+    const { value: balanceOf } = useTokenBalance(token)
     const erc721Contract = useERC721TokenContract(token?.address ?? '')
+
+    const calls = [] as string[]
+
     return useAsync(async () => {
         if (!account) return
         if (!token?.address) return
         if (token.type !== EthereumTokenType.ERC721) return
         if (!erc721Contract) return
-        return erc721Contract.methods.tokenOfOwnerByIndex(account, index).call()
-    }, [account, chainId /* re-calc when switch the chain */, token, erc721Contract, index])
+        if (balanceOf === '0') return
+    }, [account, chainId /* re-calc when switch the chain */, token, balanceOf, erc721Contract])
 }
--- packages/maskbook/src/web3/hooks/useToken.ts
+++ packages/maskbook/src/web3/hooks/useToken.ts
@@ -4,6 +4,7 @@ import { useERC20TokenContract } from '../contracts/useERC20TokenContract'
 import { Token, EthereumTokenType } from '../types'
 import { useChainId } from './useChainState'
 import { formatChecksumAddress } from '../../plugins/Wallet/formatter'
+import { useERC721TokenContract } from '../contracts/useERC721TokenContract'
 
 function resolveSettleResult<T>(result: PromiseSettledResult<T>, fallback: T) {
     return result.status === 'fulfilled' ? result.value : fallback
@@ -16,47 +17,61 @@ function resolveSettleResult<T>(result: PromiseSettledResult<T>, fallback: T) {
 export function useToken(token?: PartialRequired<Token, 'address' | 'type'>) {
     const chainId = useChainId()
     const erc20Contract = useERC20TokenContract(token?.address ?? '')
+    const erc721Contract = useERC721TokenContract(token?.address ?? '')
 
     return useAsyncRetry(async () => {
         if (!token?.address) return
 
-        // Ether
-        if (token.type === EthereumTokenType.Ether)
-            return {
-                type: EthereumTokenType.Ether,
-                chainId: token.chainId ?? chainId,
-                address: token.address,
-                name: 'Ether',
-                symbol: 'ETH',
-                decimals: 18,
-            } as Token
+        switch (token.type) {
+            case EthereumTokenType.Ether:
+                return {
+                    type: EthereumTokenType.Ether,
+                    chainId: token.chainId ?? chainId,
+                    address: token.address,
+                    name: 'Ether',
+                    symbol: 'ETH',
+                    decimals: 18,
+                } as Token
+            case EthereumTokenType.ERC20:
+                if (!erc20Contract) return
+                {
+                    // TODO:
+                    // fetch token info with multicall
+                    const [name_, symbol_, decimals_] = await Promise.allSettled([
+                        erc20Contract.methods.name().call(),
+                        erc20Contract.methods.symbol().call(),
+                        erc20Contract.methods.decimals().call(),
+                    ])
+                    return {
+                        type: EthereumTokenType.ERC20,
+                        chainId: token.chainId ?? chainId,
+                        address: formatChecksumAddress(token.address),
+                        name: resolveSettleResult(name_, token.name ?? ''),
+                        symbol: resolveSettleResult(symbol_, token.symbol ?? ''),
+                        decimals: Number.parseInt(resolveSettleResult(decimals_, String(token.decimals ?? 0)), 10),
+                    } as Token
+                }
+            case EthereumTokenType.ERC721:
+                if (!erc721Contract) return
+                {
+                    // TODO:
+                    // fetch token info with multicall
+                    const [name_, symbol_] = await Promise.allSettled([
+                        erc721Contract.methods.name().call(),
+                        erc721Contract.methods.symbol().call(),
+                    ])
 
-        // validate address
-        if (!EthereumAddress.isValid(token.address)) return
-
-        // ERC20
-        if (token.type === EthereumTokenType.ERC20) {
-            if (!erc20Contract) return
-
-            // TODO:
-            // fetch token info with multicall
-            const [name_, symbol_, decimals_] = await Promise.allSettled([
-                erc20Contract.methods.name().call(),
-                erc20Contract.methods.symbol().call(),
-                erc20Contract.methods.decimals().call(),
-            ])
-            return {
-                type: EthereumTokenType.ERC20,
-                chainId: token.chainId ?? chainId,
-                address: formatChecksumAddress(token.address),
-                name: resolveSettleResult(name_, token.name ?? ''),
-                symbol: resolveSettleResult(symbol_, token.symbol ?? ''),
-                decimals: Number.parseInt(resolveSettleResult(decimals_, String(token.decimals ?? 0)), 10),
-            } as Token
+                    return {
+                        type: EthereumTokenType.ERC721,
+                        chainId: token.chainId ?? chainId,
+                        address: formatChecksumAddress(token.address),
+                        name: resolveSettleResult(name_, token.name ?? ''),
+                        symbol: resolveSettleResult(symbol_, token.symbol ?? ''),
+                        decimals: 0,
+                    }
+                }
+            default:
+                throw new Error('not supported')
         }
-
-        // TODO:
-        // ERC721
-        throw new Error('not supported')
-    }, [chainId, token?.type, token?.address, erc20Contract])
+    }, [chainId, token?.type, token?.address, erc20Contract, erc721Contract])
 }
